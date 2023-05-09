73c7d895527344151c9264aee7c21dbe769834df
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-05-09 21:20:25 +0000

    Se realiza el semaforo como se es solicitado.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState }  from "react";
 //create your first component
 const Home = () => {
+	
+	const [redLight, setRedLight] = useState()
+	const [yellowLight, setYellowLight] = useState()
+	const [greenLight, setGreenLight] = useState()
+
+	function red (){
+		if (redLight === ''){
+			setRedLight('brillo')
+		} else setRedLight('')
+	 	setYellowLight('')
+		setGreenLight('')
+	}
+
+	function yellow (){
+		if (yellowLight === ''){
+			setYellowLight('brillo')
+		} else setYellowLight('')
+	 	setRedLight('')
+		setGreenLight('')
+	}
+
+	function green (){
+		if (greenLight === ''){
+			setGreenLight('brillo')
+		} else setGreenLight('')
+	 	setRedLight('')
+		setYellowLight('')
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		<div className="traffic-top bg-dark"></div>
+			<div className="traffic-light bg-dark">
+				<div onClick={red} className={"light red " + redLight}></div>
+				<div onClick={yellow} className={"light yellow " + yellowLight}></div>
+				<div onClick={green} className={"light green " + greenLight}></div>
+			</div>
+		</>
 	);
 };
-
+ 
 export default Home;
