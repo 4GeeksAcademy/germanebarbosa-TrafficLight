568192f6058435705e801f12ad33b738070620b9
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-05-11 22:10:33 +0000

    Se agrega luz purpura mediante un boton.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
 	const [redLight, setRedLight] = useState('')
 	const [yellowLight, setYellowLight] = useState('')
 	const [greenLight, setGreenLight] = useState('')
+	const [purpleLight, setPurpleLight] = useState('')
+	const [mostrarComponente, setMostrarComponente] = useState(false);
+
 
 	function red (){
 		console.log('Se ejecuto red')
@@ -14,6 +17,7 @@ const Home = () => {
 		}
 	 	setYellowLight('')
 		setGreenLight('')
+		setPurpleLight('')
 	}
 
 	function yellow (){
@@ -23,6 +27,7 @@ const Home = () => {
 		} else setYellowLight('')
 	 	setRedLight('')
 		setGreenLight('')
+		setPurpleLight('')
 	}
 
 	function green (){
@@ -32,16 +37,28 @@ const Home = () => {
 		} else setGreenLight('')
 	 	setRedLight('')
 		setYellowLight('')
+		setPurpleLight('')
+	}
+
+	function purple (){
+		console.log('Se ejecuto purple')
+		if (purpleLight == ''){
+			setPurpleLight('brillo')
+		} else setPurpleLight('')
+	 	setRedLight('')
+		setYellowLight('')
+		setGreenLight('')
 	}
 
 	function apagarLuces(){
 		setRedLight('')
 		setYellowLight('')
 		setGreenLight('')
+		setPurpleLight('')
 	}
 
 	function cambioLuces(){
-		if(redLight=='brillo' || yellowLight=='brillo' || greenLight =='brillo'){
+		if(redLight=='brillo' || yellowLight=='brillo' || greenLight =='brillo' || purpleLight == 'brillo'){
 			apagarLuces()} else{
 				setTimeout(red,500)
 				// console.log(redLight)
@@ -49,6 +66,7 @@ const Home = () => {
 				setTimeout(yellow,1000)
 				// console.log(yellowLight  + ' despues')
 				setTimeout(green,1500)
+				setTimeout(purple,2000)
 			}
 	}
 
@@ -59,8 +77,10 @@ const Home = () => {
 				<div onClick={red} className={"light red " + redLight}></div>
 				<div onClick={yellow} className={"light yellow " + yellowLight}></div>
 				<div onClick={green} className={"light green " + greenLight}></div>
+				<div onClick={purple} className={(mostrarComponente) ? "show-element" && "light purple " + purpleLight : null}></div>
 			</div>
 			<button type="button" className="btn btn-dark boton" onClick={cambioLuces}>Cambiar luces</button>
+			<button type="button" className="btn btn-dark boton" onClick={() => setMostrarComponente(!mostrarComponente)}>{mostrarComponente ? `Ocultar` : `Mostrar`} luz purpura</button>
 		</>
 	);
 };
