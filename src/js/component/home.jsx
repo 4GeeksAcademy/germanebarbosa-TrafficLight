c9a25a238b89b453cc688cc203346557ad52e255
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-05-11 03:13:05 +0000

    Se agrega boton que prende las luces

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,24 @@
-
 import React, { useState }  from "react";
 //create your first component
 const Home = () => {
 	
-	const [redLight, setRedLight] = useState()
-	const [yellowLight, setYellowLight] = useState()
-	const [greenLight, setGreenLight] = useState()
+	const [redLight, setRedLight] = useState('')
+	const [yellowLight, setYellowLight] = useState('')
+	const [greenLight, setGreenLight] = useState('')
 
 	function red (){
-		if (redLight === ''){
+		console.log('Se ejecuto red')
+		if (redLight == ''){
 			setRedLight('brillo')
-		} else setRedLight('')
+		} else{ setRedLight('')
+		}
 	 	setYellowLight('')
 		setGreenLight('')
 	}
 
 	function yellow (){
-		if (yellowLight === ''){
+		console.log('Se ejecuto yellow')
+		if (yellowLight == ''){
 			setYellowLight('brillo')
 		} else setYellowLight('')
 	 	setRedLight('')
@@ -24,11 +26,30 @@ const Home = () => {
 	}
 
 	function green (){
-		if (greenLight === ''){
+		console.log('Se ejecuto green')
+		if (greenLight == ''){
 			setGreenLight('brillo')
 		} else setGreenLight('')
 	 	setRedLight('')
 		setYellowLight('')
+	}
+
+	function apagarLuces(){
+		setRedLight('')
+		setYellowLight('')
+		setGreenLight('')
+	}
+
+	function cambioLuces(){
+		if(redLight=='brillo' || yellowLight=='brillo' || greenLight =='brillo'){
+			apagarLuces()} else{
+				setTimeout(red,500)
+				// console.log(redLight)
+				// console.log(yellowLight + ' antes')
+				setTimeout(yellow,1000)
+				// console.log(yellowLight  + ' despues')
+				setTimeout(green,1500)
+			}
 	}
 
 	return (
@@ -39,6 +60,7 @@ const Home = () => {
 				<div onClick={yellow} className={"light yellow " + yellowLight}></div>
 				<div onClick={green} className={"light green " + greenLight}></div>
 			</div>
+			<button type="button" className="btn btn-dark boton" onClick={cambioLuces}>Cambiar luces</button>
 		</>
 	);
 };
